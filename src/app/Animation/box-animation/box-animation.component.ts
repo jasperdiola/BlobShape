c10985cf127cb3b8d27d657bc10aa3dfba3595d2
@jasperdiola <jasperdiola@yahoo.com>
2023-06-26 16:15:43 -0700

    added new Feature

--- src/app/Animation/box-animation/box-animation.component.ts
+++ src/app/Animation/box-animation/box-animation.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-box-animation',
   templateUrl: './box-animation.component.html',
   styleUrls: ['./box-animation.component.scss']
 })
-export class BoxAnimationComponent {
+export class BoxAnimationComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('blobShape') blobShape!:ElementRef;
+
+  constructor(private renderer: Renderer2){}
+
+  ngAfterViewInit() {
+    
+  }
+
+  ngOnInit(){
+
+  }
+
+  boolVal:boolean = true;
+  onClick(){
+    const element = this.blobShape.nativeElement;
+
+    if (this.boolVal) {
+      this.renderer.setStyle(element, 'border-bottom', '20px solid white')
+      this.boolVal = !this.boolVal;
+    } else {
+      this.renderer.setStyle(element, 'border-bottom', '0px solid white')
+      this.boolVal = !this.boolVal;
+    }
+  }
+
+  
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoxAnimationComponent } from './Animation/box-animation/box-animation.component';
+import { HeaderComponent } from './share/header/header.component';
+import { FooterComponent } from './share/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoxAnimationComponent
+    BoxAnimationComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
