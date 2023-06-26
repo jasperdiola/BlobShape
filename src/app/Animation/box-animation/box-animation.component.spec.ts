60c62403239f3b96a9ef7c9f31eb73ae0cbb7d84
@jasperdiola <jasperdiola@yahoo.com>
2023-06-26 11:56:50 -0700

    added another layer

---
+++ src/app/Animation/box-animation/box-animation.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoxAnimationComponent } from './box-animation.component';
+
+describe('BoxAnimationComponent', () => {
+  let component: BoxAnimationComponent;
+  let fixture: ComponentFixture<BoxAnimationComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BoxAnimationComponent]
+    });
+    fixture = TestBed.createComponent(BoxAnimationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/Animation/box-animation/box-animation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box-animation',
+  templateUrl: './box-animation.component.html',
+  styleUrls: ['./box-animation.component.scss']
+})
+export class BoxAnimationComponent {
+
+}
